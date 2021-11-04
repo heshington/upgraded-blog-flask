63ec4d387b0d79e49fe9c44c59e296b0940d9e6c
heshington <hiren6@gmail.com>
2021-11-04 22:23:48 +1300

    Link to single blog post works.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-from datetime import date
 import requests
 app = Flask(__name__)
 
@@ -20,6 +19,20 @@ def about():
 def contact():
     return render_template("contact.html")
 
+@app.route('/post/<int:id>')
+def get_blog(id):
+    api_endpoint = "https://api.npoint.io/e75e0e49fccb076f6e84"
+    blogs = requests.get(api_endpoint)
+    blog_data = blogs.json()
+    for blog in blog_data:
+        if blog['id'] == id:
+            title = blog['title']
+            subtitle = blog['subtitle']
+            author = blog['author']
+            image = blog['image']
+            date = blog['date']
+            body = blog['body']
+    return render_template('post.html', title=title,subtitle=subtitle, author=author, image=image, date=date, blog=body)
 
 if __name__ == "__main__":
     app.run(debug=True)
